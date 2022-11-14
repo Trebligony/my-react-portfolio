3614b6c2c1bba8c20d8bd0842768067379525ea3
Ala Owerri <irete@alaowerri.com>
2022-11-13 20:08:24 -0500

    adding index.js file in src/components/footer folder

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div class="footer" id="footer">
+        <a href="https://github.com/Trebligony/"><img src="https://img.icons8.com/color/48/000000/github.png" alt="Github" class="icon"/></a>
+        <a href="https://www.linkedin.com/in/gilbert-onyenwezi-71853531/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
+        <a href="https://twitter.com/Gillonye/"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" class="icon"/></a>
+        <a href="https://stackoverflow.com/users/5762371/gillonye/"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" class="icon"/></a>
+        <p>
+        copyright &copy; Gilbert Onyenwezi 
+        </p>
+
+    </div>
+    )
+}
+
+export default Footer;

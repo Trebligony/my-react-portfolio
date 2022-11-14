97cf846a48b6d6f3ae99329eb179c677585797dd
Ala Owerri <irete@alaowerri.com>
2022-11-13 20:06:50 -0500

    adding index.ja file in src/about folder

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+    return(
+    <section className="title">
+      <h1 class="name">about: Gilbert Onyenwezi</h1>
+      <hr></hr>
+      
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+            <img class="mb-5" src="/images/Gill_logo.png" alt="Gilbert Onyenwezi"/>
+        <p>I am a problem solvers who enjoy thinking outside the box and working in a dynamic world. Sounds interesting? That is only the start because, 43% of developers have a bachelor’s degree in computer science or a similar field, 69% claim to be partially self-taught.</p>
+      <p>An executive with over fifteen years of success driving profits, quality, and customer satisfaction to record levels. 
+Background includes operations leadership roles at Amazon, United Geophysical, Ferriesmart and Manaksia 
+Industries. Overseeing multi-shift, multi-plant operations, including experiences in manufacturing, production & 
+supply chain environments with emphasis on project coordination and management.</p>
+      <p>
+            View full <a href="#/resume" class="link">resume</a>
+    </p>
+      </div>  
+      </div>
+    </section> 
+)}
+  
+export default About;

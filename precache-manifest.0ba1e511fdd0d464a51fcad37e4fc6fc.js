3476546185e8e2551f99eeaeec8b4a9c35c23a2f
Ala Owerri <irete@alaowerri.com>
2022-11-14 22:10:50 -0500

    Updates

---
+++ precache-manifest.0ba1e511fdd0d464a51fcad37e4fc6fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df4e9731994ccd37e413842f7d11cbb",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "7d81b9eb1f5a65f64a99",
+    "url": "/my-react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6057ebbe6da7789204bf",
+    "url": "/my-react-portfolio/static/css/main.e0c3848b.chunk.css"
+  },
+  {
+    "revision": "7d81b9eb1f5a65f64a99",
+    "url": "/my-react-portfolio/static/js/2.57f34e8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-portfolio/static/js/2.57f34e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6057ebbe6da7789204bf",
+    "url": "/my-react-portfolio/static/js/main.5f74f718.chunk.js"
+  },
+  {
+    "revision": "a09521d6aa79f251d523",
+    "url": "/my-react-portfolio/static/js/runtime-main.bdbff3dd.js"
+  }
+]);
